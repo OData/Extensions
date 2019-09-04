6e3de86f1ae3de40644ed7085a16156bafea5a55
Chuanbo Zhang <chuanboz@microsoft.com>
2019-09-03 17:04:53 -0700

    Update TaskExtensionMethods.cs
    fix https://github.com/OData/Extensions/issues/12 by running continuation synchronizely.

--- src/Microsoft.Extensions.OData.Client/Internals/TaskExtensionMethods.cs
+++ src/Microsoft.Extensions.OData.Client/Internals/TaskExtensionMethods.cs
@@ -24,7 +24,7 @@ namespace Microsoft.Extensions.OData.Client
         /// <param name="state">The APM state to inject.</param>
         /// <returns>A Task that has been adapted to use the APM pattern.</returns>
         /// <remarks>Taken from http://blogs.msdn.com/b/pfxteam/archive/2011/06/27/10179452.aspx </remarks>
-        public static Task<TResult> ToApm<TResult>(this Task<TResult> task, AsyncCallback callback, object state)
+        internal static Task<TResult> ToApm<TResult>(this Task<TResult> task, AsyncCallback callback, object state)
         {
             var tcs = new TaskCompletionSource<TResult>(state);
 
@@ -47,7 +47,9 @@ namespace Microsoft.Extensions.OData.Client
                     callback?.Invoke(tcs.Task);
                 },
                 CancellationToken.None,
-                TaskContinuationOptions.None,
+                // run continuation sync to avoid high latency issues due to context switch under high load.
+                // see https://github.com/OData/Extensions/issues/12 for more details.
+                TaskContinuationOptions.ExecuteSynchronously,
                 TaskScheduler.Default);
 
             return tcs.Task;
