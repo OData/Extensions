43f008682d4b11aa5e916cebd83beaa61eda8b3b
groovyfeng <groovyfeng@gmail.com>
2019-09-30 12:46:42 -0600

    OData Migration

---
+++ src/Microsoft.Extensions.OData.Migration/Formatters/Serialization/ODataMigrationCollectionSerializer.cs
@@ -0,0 +1,44 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright company="Microsoft Corporation">
+//     Copyright © Microsoft Corporation. All rights reserved.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+namespace Microsoft.Extensions.OData.Migration.Formatters.Serialization
+{
+    using Microsoft.AspNet.OData.Formatter.Serialization;
+    using Microsoft.OData;
+    using Microsoft.OData.Edm;
+    using System;
+
+    /// <summary>
+    /// Customized serializer that converts V3 incompatible types to compatible types in collections
+    /// </summary>
+    internal class ODataMigrationCollectionSerializer : ODataCollectionSerializer
+    {
+        public ODataMigrationCollectionSerializer(ODataSerializerProvider provider)
+            : base(provider)
+        {
+        }
+
+        public override void WriteObject(object graph, Type type, ODataMessageWriter messageWriter, ODataSerializerContext writeContext)
+        {
+            if (messageWriter == null)
+            {
+                throw new ArgumentNullException(nameof(messageWriter));
+            }
+
+            if (writeContext == null)
+            {
+                throw new ArgumentNullException(nameof(writeContext));
+            }
+
+            IEdmTypeReference collectionType = writeContext.GetEdmType(graph, type);
+
+            // Translate types in response stream according to expected collection type
+            messageWriter.PreemptivelyTranslateResponseStream(
+               collectionType,
+               (writer) => base.WriteObject(graph, type, writer, writeContext));
+        }
+    }
+}
