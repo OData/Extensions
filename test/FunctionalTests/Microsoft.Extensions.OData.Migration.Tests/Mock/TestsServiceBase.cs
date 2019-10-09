5c9a0398dd2d1d3ac39e993382cd223c0d3739c7
groovyfeng <groovyfeng@gmail.com>
2019-10-08 18:09:15 -0600

    Addressing code review feedback

--- test/FunctionalTests/Microsoft.Extensions.OData.Migration.Tests/Mock/TestsServiceBase.cs
+++ test/FunctionalTests/Microsoft.Extensions.OData.Migration.Tests/Mock/TestsServiceBase.cs
@@ -6,10 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.Extensions.OData.Migration.Tests.Mock
 {
