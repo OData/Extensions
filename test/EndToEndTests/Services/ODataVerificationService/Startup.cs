80ff21272de6072eaa3a8f03fca96a085e018ec3
John Gathogo <john.gathogo@microsoft.com>
2022-11-02 16:04:38 +0300

    Upgrade NETCore version to 2.2

--- test/EndToEndTests/Services/ODataVerificationService/Startup.cs
+++ test/EndToEndTests/Services/ODataVerificationService/Startup.cs
@@ -28,8 +28,11 @@ namespace ODataVerificationService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddMvc(options => options.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddApplicationPart(this.GetType().Assembly);
             services.AddOData();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
