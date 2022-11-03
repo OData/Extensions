5a76a18cfff778d2b8aaebe3784773245bd6dbc0
John Gathogo <john.gathogo@microsoft.com>
2022-11-03 10:34:42 +0300

    Upgrade test projects to .NET Core 3.1

--- test/EndToEndTests/Services/ODataVerificationService/Startup.cs
+++ test/EndToEndTests/Services/ODataVerificationService/Startup.cs
@@ -30,13 +30,13 @@ namespace ODataVerificationService
         {
             services
                 .AddMvc(options => options.EnableEndpointRouting = false)
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
                 .AddApplicationPart(this.GetType().Assembly);
             services.AddOData();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMvc(b =>
             {
